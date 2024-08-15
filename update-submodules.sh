967dec96c7f97910f107f8381a095047c285201e
Jimmy Ihalainen <jimmy.ihalainen@gmail.com>
2024-08-15 10:59:57 +0300

    Improve update-submodules.sh

--- update-submodules.sh
+++ update-submodules.sh
@@ -2,6 +2,12 @@
 
 echo "Updating submodules..."
 
+for dir in a-plus mooc-grader gitmanager mooc-jutut aplus-manual; do
+    cd "$dir"
+    git switch master > /dev/null 2>&1
+    cd ..
+done
+
 git submodule update --init --remote
 
 echo "Done!"
